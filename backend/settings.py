6101b594c6707714c2548b7986ce674bc370871f
Pranav-496 <pranavlandge79@gmail.com>
2026-04-05 21:53:11 +0530

    allowed hosts

--- backend/settings.py
+++ backend/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-secret-key")
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = ["ecommerce-backend-1-nxnh.onrender.com", 'localhost', '127.0.0.1']
 
 # =========================
 # APPLICATIONS
